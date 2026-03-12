8a81d64d0efbd387e0989363a9013387f042e7d3
darelleh12 <darelleh12@gmail.com>
2026-03-12 13:20:37 -0500

    make solve file

---
+++ solve.py
@@ -0,0 +1,10 @@
+#solve maze here
+#do we want do bo backtracking?
+#The maze is internally represented as a 64×64 integer array:
+# • 0 = Empty/navigable cell
+# • 1 = Wall
+# • 2 = Start position
+# • 3 = Goal position
+# • 4 = Death pit
+# • 5 = Teleport pad
+
